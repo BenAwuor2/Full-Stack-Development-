7d0cf7d59149d5dea03a1ead3732894064abbe20
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2023-11-20 10:32:12 +0300

    cookies

--- node-express-jwt-auth-lesson-1/models/Users.js
+++ node-express-jwt-auth-lesson-1/models/Users.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
+
 
 const userSchema = new mongoose.Schema({
     email : {
@@ -15,6 +17,19 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6-xters'],
     }
 });
+
+//Fire a fucntion after user is saved on DB
+// userSchema.post("save", function(doc,next) {
+//     console.log("New user was created!", doc);
+//     next();
+// })
+
+//Fire a fucntion before user is saved on DB
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
 const user = mongoose.model('user', userSchema);
 
