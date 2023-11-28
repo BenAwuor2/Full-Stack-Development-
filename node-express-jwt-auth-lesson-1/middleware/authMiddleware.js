8e6e73667be56567c1b4e959d9a26b6950c600b7
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2023-11-28 19:22:37 +0300

    user

---
+++ node-express-jwt-auth-lesson-1/middleware/authMiddleware.js
@@ -0,0 +1,91 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/Users');
+
+
+const requireAuth = ( req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, "secrate", (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect('/login');
+            }
+            else {
+                console.log(decodedToken);
+                next();
+            }
+        });
+    }
+    else{
+        res.redirect('/login');
+    }
+
+}
+
+// Check current user
+
+// const checkUser = function (req, res, next) {
+//     const token = req.cookies.jwt;
+    
+
+//     if (token) {
+//         jwt.verify(token, "secrate", async (err, decodedToken) => {
+//             if (err) {
+//                 console.log(err.message);
+//                 res.locals.user = null;
+//                 next();
+//             }
+//             else {
+//                 console.log(decodedToken);
+
+//                 let user = await User.findById(decodedToken.id);
+//                 res.locals.user = user;
+//                 next();
+//             }
+//         });
+//     }
+//     else {
+//         res.locals.user = null;
+//         next();
+//     }
+// }
+
+// Check current user
+const checkUser = async function (req, res, next) {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+
+                try {
+                    let user = await User.findById(decodedToken.id);
+
+                    if (!user) {
+                        throw new Error('User not found');
+                    }
+
+                    res.locals.user = user;
+                    next();
+                } catch (error) {
+                    console.error(error.message);
+                    res.locals.user = null;
+                    next();
+                }
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
+
+
